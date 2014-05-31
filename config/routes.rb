103e77ccb89e1894d8bc7efad831d11eeec093bf
Sere <questioare@gmail.com>
2014-05-31 17:05:02 +0300

    Awesome app
    Ruby on Rails
    edit, add images
    css styles
    pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Railsgirls::Application.routes.draw do
+  resources :ideas
+
+  get "page/info"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
